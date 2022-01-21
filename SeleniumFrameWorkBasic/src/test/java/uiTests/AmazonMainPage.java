675be6c88206dff749aefafa79193f57bd307b87
maciu <maciu@DESKTOP-PJ8TGSP>
2022-01-21 14:39:37 +0200

    Work1

---
+++ SeleniumFrameWorkBasic/src/test/java/uiTests/AmazonMainPage.java
@@ -0,0 +1,62 @@
+package uiTests;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class AmazonMainPage {
+	
+	public static String browserName = "chrome";
+	
+	WebDriver driver;
+	
+	@BeforeTest
+	
+	public void SetUpTest() {
+		
+		if (browserName.equalsIgnoreCase("chrome")) {
+			
+			WebDriverManager.chromedriver().setup();
+			
+			//WebDriver driver = new ChromeDriver(); --won't work
+			driver = new ChromeDriver();
+			driver.get("https://www.amazon.com/");
+			
+		}
+		
+		else if(browserName.equalsIgnoreCase("firefox")) {
+			
+			WebDriverManager.firefoxdriver().setup();
+			driver = new FirefoxDriver();
+			driver.get("https://www.amazon.com/");
+			
+		}
+	}
+	
+
+	@Test
+	public void myMethod() {
+		
+	
+		WebElement textsearch = driver.findElement(By.id("twotabsearchtextbox"));
+		
+		textsearch.sendKeys("portocala");
+		
+		
+
+	}
+
+	@AfterTest
+	
+	public void tearDown() {
+		
+		driver.close();
+	}
+
+}
