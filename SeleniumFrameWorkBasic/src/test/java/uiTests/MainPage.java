89fc153821f1b3756f2d144b86cedd86d5be80ad
maciu <maciu@DESKTOP-PJ8TGSP>
2022-01-21 12:58:10 +0200

    Update

--- SeleniumFrameWorkBasic/src/test/java/uiTests/MainPage.java
+++ SeleniumFrameWorkBasic/src/test/java/uiTests/MainPage.java
@@ -1,5 +1,7 @@
 package uiTests;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -13,6 +15,13 @@ public class MainPage {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.com/");
+		
+		WebElement textsearch = driver.findElement(By.id("twotabsearchtextbox"));
+		
+		textsearch.sendKeys("portocala");
+		
+		
+		
 
 
 	}
