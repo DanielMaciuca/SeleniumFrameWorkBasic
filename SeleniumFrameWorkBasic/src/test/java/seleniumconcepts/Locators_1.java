7201c198426b33c45d3a006669dd61beb564d1ee
maciu <maciu@DESKTOP-PJ8TGSP>
2022-01-27 12:57:01 +0200

    locators 2

---
+++ SeleniumFrameWorkBasic/src/test/java/seleniumconcepts/Locators_1.java
@@ -0,0 +1,68 @@
+package seleniumconcepts;
+
+import java.time.Duration;
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Locators_1 {
+
+	public static void main(String[] args) {
+
+		WebDriverManager.chromedriver().setup();
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.emag.ro/");
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+		// By.ID
+
+		//driver.findElement(By.id("searchboxTrigger")).click();
+
+		// By.NAME
+
+		//driver.findElement(By.name("query")).click();
+
+		// By.CLASS
+
+		driver.manage().window().maximize();
+
+		//driver.findElement(By.className("caret")).click();
+
+		//By.TAGNAME - Verifying the number of links from a page
+
+		//driver.findElement(By.tagName("img")).click();
+
+		/*
+		 * List<WebElement> links= driver.findElements(By.tagName("a"));
+		 * 
+		 * int i= links.size();
+		 * 
+		 * System.out.println(i);
+		 * 
+		 * for(int j=0; j<i; j++) { //Printing the links
+		 * System.out.println(links.get(j).getText()); }
+		 */
+
+		// By.LINKTEXT
+
+		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+		//driver.findElement(By.linkText("eMAG Genius")).click();
+		
+		//By.PARTIALLINKTEXT
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+
+		//driver.findElement(By.partialLinkText("eMAG")).click();
+		
+		driver.findElement(By.partialLinkText("PC")).click();
+	}
+
+}
---
+++ SeleniumFrameWorkBasic/src/test/java/seleniumconcepts/Locators_xPath_css_Selector.java
@@ -0,0 +1,10 @@
+package seleniumconcepts;
+
+public class Locators_xPath_css_Selector {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
