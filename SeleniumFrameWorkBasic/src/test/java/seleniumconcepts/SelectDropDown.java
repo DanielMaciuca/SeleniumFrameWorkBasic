97d6d4a4ccd03ea0253112a722ae29e8f8f5e71e
maciu <maciu@DESKTOP-PJ8TGSP>
2022-02-03 13:45:06 +0200

    drop down

--- SeleniumFrameWorkBasic/src/test/java/seleniumconcepts/SelectDropDown.java
+++ SeleniumFrameWorkBasic/src/test/java/seleniumconcepts/SelectDropDown.java
@@ -1,9 +1,36 @@
 package seleniumconcepts;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class SelectDropDown {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		WebDriverManager.chromedriver().setup();
+
+		WebDriver driver = new ChromeDriver();
+
+		driver.get("https://www.amazon.com/");
+
+		WebElement element = driver.findElement(By.xpath("//select[@id='searchDropdownBox']"));
+
+		Select dropdown = new Select(element);
+
+		//dropdown.selectByIndex(3);
+
+		dropdown.selectByValue("search-alias=baby-products-intl-ship"); 
+
+		//dropdown.selectByVisibleText("Baby");
+
+		//dropdown.deselectByIndex(1);
+		//dropdown.deselectByValue("search-alias=baby-products-intl-ship");
+		dropdown.deselectByVisibleText("Baby");
 
 	}
 
