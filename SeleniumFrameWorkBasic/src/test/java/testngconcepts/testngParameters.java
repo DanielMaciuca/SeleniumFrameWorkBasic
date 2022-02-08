c72883b2d5054544804525804d6b6849bf33a61a
maciu <maciu@DESKTOP-PJ8TGSP>
2022-02-08 16:17:01 +0200

    param

---
+++ SeleniumFrameWorkBasic/src/test/java/testngconcepts/testngParameters.java
@@ -0,0 +1,26 @@
+package testngconcepts;
+
+import org.testng.annotations.Optional;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+
+public class testngParameters {
+	
+	@Test
+	@Parameters({"Name", "Age"})
+	public void method(String nume, int varsta) {
+
+		System.out.println(nume + varsta);
+
+	}
+
+	@Test
+	@Parameters({"SecondName"})
+	public void method2(@Optional("Maciuca") String nume) {
+
+		System.out.println(nume);
+
+	}
+}
