4a868749d71d3a5c0b703a9a803c60ca86046bab
Daniel Maciuca <maciuca_d@yahoo.com>
2022-02-28 13:43:48 +0200

    Log4j bash

--- SeleniumFrameWorkBasic/src/test/java/Log4j/Log4jDemo.java
+++ SeleniumFrameWorkBasic/src/test/java/Log4j/Log4jDemo.java
@@ -1,9 +1,21 @@
 package Log4j;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Log4jDemo {
 
+	static Logger logger = LogManager.getFormatterLogger(Log4jDemo.class);
+
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		//System.out.println("this is first line");
+		//It only prints the error message and the fatal message because there is not configuration file defined
+
+		logger.info("This is info message");
+		logger.error("This is an error message");
+		logger.warn("This is a warning");
+		logger.fatal("This is a fatal error");
 
 	}
 
