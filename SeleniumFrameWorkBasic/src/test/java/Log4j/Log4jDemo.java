7daea8d7d71fcfd1ddcab0ab114701ebce6a2f36
Daniel Maciuca <maciuca_d@yahoo.com>
2022-02-28 15:52:03 +0200

    log4j2

--- SeleniumFrameWorkBasic/src/test/java/Log4j/Log4jDemo.java
+++ SeleniumFrameWorkBasic/src/test/java/Log4j/Log4jDemo.java
@@ -2,21 +2,26 @@ package Log4j;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Log4jDemo {
 
 	static Logger logger = LogManager.getFormatterLogger(Log4jDemo.class);
 
-	public static void main(String[] args) {
+	@Test
 
-		//System.out.println("this is first line");
-		//It only prints the error message and the fatal message because there is not configuration file defined.
+	public void method() {
+
+		System.out.println("Only a static method");
 
 		logger.info("This is info message");
 		logger.error("This is an error message");
 		logger.warn("This is a warning");
 		logger.fatal("This is a fatal error");
-
 	}
-
 }
