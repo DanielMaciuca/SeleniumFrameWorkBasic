ce2c253a7634873afa6fc3f27ec87d5e8016225b
Daniel Maciuca <maciuca_d@yahoo.com>
2022-03-07 18:34:34 +0200

    js alert2

--- SeleniumFrameWorkBasic/src/test/java/seleniumconcepts/JavaScriptAlert.java
+++ SeleniumFrameWorkBasic/src/test/java/seleniumconcepts/JavaScriptAlert.java
@@ -2,6 +2,7 @@ package seleniumconcepts;
 
 import java.time.Duration;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -17,19 +18,36 @@ public class JavaScriptAlert {
 
 		driver.get("http://the-internet.herokuapp.com/javascript_alerts");
 
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		// We can handle alerts in Selenium webdriver by using the Alert interface
+		// It works directly  without using the Alert interface, maybe is only for handling the code better
+		// Also works with driver.switchTo().alert().accept() directly
 
-		driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();
+		//simple alert -> displays some information or warning
 
-		//simple alert -> display some information or warning
-		driver.switchTo().alert().accept();
+		driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();	
 
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		Alert myAlert1 = driver.switchTo().alert();
 
-		driver.findElement(By.xpath("//button[text()='Click for JS Alert']")).click();
-		
-		//confirmation alert -> ask permission to do something
+		myAlert1.accept();
 
+		//confirmation alert -> asks permission to do something, Cancel is pressed
+
+		driver.findElement(By.xpath("//button[text()='Click for JS Confirm']")).click();
+
+		Alert myAlert2 = driver.switchTo().alert();
+
+		myAlert2.dismiss();
+
+		//prompt alert -> asks input from the user
+
+		driver.findElement(By.xpath("//button[text()='Click for JS Prompt']")).click();
+
+		Alert myAlert3 = driver.switchTo().alert();
+
+		//DOESN'T WORK
+		myAlert3.sendKeys("This is as test");
+
+		//System.out.println(myAlert3.getText());
 	}
 
 }
