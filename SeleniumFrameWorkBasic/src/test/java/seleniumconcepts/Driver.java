a173fa7433d11f9707c6417edf0441076a96ba4a
Daniel Maciuca <maciuca_d@yahoo.com>
2024-02-13 21:39:54 +0200

    new

---
+++ SeleniumFrameWorkBasic/src/test/java/seleniumconcepts/Driver.java
@@ -0,0 +1,19 @@
+package seleniumconcepts;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Driver {
+
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("https://www.amazon.com/");
+
+    }
+}
