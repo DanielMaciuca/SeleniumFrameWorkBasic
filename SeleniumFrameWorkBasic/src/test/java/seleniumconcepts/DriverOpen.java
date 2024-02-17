c373679d362973691ed04fee2c39996f2ba63c35
Daniel Maciuca <maciuca_d@yahoo.com>
2024-02-17 15:42:35 +0200

    new

--- SeleniumFrameWorkBasic/src/test/java/seleniumconcepts/DriverOpen.java
+++ SeleniumFrameWorkBasic/src/test/java/seleniumconcepts/DriverOpen.java
@@ -1,6 +1,11 @@
 package seleniumconcepts;
 
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
 public class DriverOpen {
 
     public static void main(String[] args) {
@@ -16,9 +21,9 @@ public class DriverOpen {
 //        WebDriver driver1 = new FirefoxDriver();
 //        driver1.get("https://www.emag.ro");
 
-//        WebDriverManager.edgedriver().setup();
-//        WebDriver driver2 = new EdgeDriver();
-//        driver2.get("https://www.emag.ro");
+        WebDriverManager.edgedriver().setup();
+        WebDriver driver2 = new EdgeDriver();
+        driver2.get("https://www.emag.ro");
 
     }
 }
--- SeleniumFrameWorkBasic/src/test/java/seleniumconcepts/SelectDropDown.java
+++ SeleniumFrameWorkBasic/src/test/java/seleniumconcepts/SelectDropDown.java
@@ -4,19 +4,26 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.Select;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.time.Duration;
 
 public class SelectDropDown {
 
 	public static void main(String[] args) {
 
 		WebDriverManager.chromedriver().setup();
-
-		WebDriver driver = new ChromeDriver();
+		ChromeOptions options = new ChromeOptions();
+		options.addArguments("--remote-allow-origins=*");
+		WebDriver driver = new ChromeDriver(options);
 
 		driver.get("https://www.amazon.com/");
+
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(6));
+
 
 		WebElement element = driver.findElement(By.xpath("//select[@id='searchDropdownBox']"));
 
